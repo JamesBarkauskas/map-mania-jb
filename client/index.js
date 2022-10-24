5bf02867f87391b96978b2dbce8d502af2f81aad
James <jimbarkauskas@gmail.com>
2022-10-24 01:10:52 -0500

    tried adding json

--- client/index.js
+++ client/index.js
@@ -1,5 +1,31 @@
 var gMap;
 
+function initApp() {
+    console.log('Map Mania starting...');
+    start();
+}
+
+async function start() {
+    try{
+        const response = await fetch('/send')
+        const data = await response.json()
+        mapPoints(data)
+    } catch(e) {
+        console.log('There was an error getting the data')
+    }
+}
+
+function mapPoints(data) {
+    var dataList = Object.values(data.places)
+    for (let i = 0; i < dataList.length; i++) {
+        var name = dataList[i].name
+        var latt = dataList[i].latt
+        var long = dataList[i].long
+        new google.maps.Marker({position:{lat:latt, lng:long},
+        map: gMap, title: name})
+    }
+}
+
 function initMap() {
     // creating a new map
     gMap = new google.maps.Map(document.getElementById('myMapID'), {
@@ -7,7 +33,7 @@ function initMap() {
     });
 
     var marker = new google.maps.Marker({position:{lat:37.5147, lng:-88.8734},
-    map:gMap});
+    map:gMap}); 
 
     google.maps.event.addListener(gMap, 'idle', function() {
         updateGame()
@@ -26,6 +52,3 @@ function updateGame() {
     console.log('inBounds:'+inBounds+' zoomLevel:'+zoomLevel);
 }
 
-function initApp() {
-    console.log('Map Mania starting...');
-}
--- index.js
+++ index.js
@@ -2,11 +2,35 @@ const express = require("express");
 const app = express();
 
 app.use(express.static(__dirname + '/client'))
+app.use(express.json());
 
 const port = process.env.PORT || 3000
-app.get('/test', function(request, response) {
+
+
+/*app.get('/test', function(request, response) {
 	response.type('text/plain')
 	response.send('Node.js and Express running on port='+port)
+}) */
+
+var favoritePlaces = {
+	"places": [
+		{"name":"Shawnee National Forest, IL", "latt": 37.5147, "long": -88.8734},
+		{"name":"Giant City State Park, IL", "latt": 37.6061, "long": -89.1859},
+		{"name":"New Orleans, LA", "latt": 29.9511, "long": -90.0715},
+		{"name":"Myrtle Beach, SC", "latt": 33.6891, "long": -78.8867},
+		{"name":"Orlando, FL", "latt": 28.5384, "long": -81.3789},
+		{"name":"Breckenridge, CO", "latt": 39.4817, "long": -106.0384},
+		{"name":"San Tan Valley, AZ", "latt": 33.1773, "long": -111.5468},
+		{"name":"Matthiessen State Park, IL", "latt": 41.2960, "long": -89.0244},
+		{"name":"San Diego, CA", "latt": 32.7157, "long": -111.1611},
+		{"name":"Nashville, TN", "latt": 36.1627, "long": -86.7816},
+		{"name":"Wisconsin Dells, WI", "latt": 43.6275, "long": -89.7710}
+	]
+}
+
+app.get('/send', function(req, res) {
+	console.log(favoritePlaces)
+	res.json(favoritePlaces)
 })
 
 app.listen(port, function() {
